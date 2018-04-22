ce866ff50a01975e77a57b06b07d5f2fe246445f
Nana Adjei Manu <claeusdev@gmail.com>
2018-04-22 15:57:18 +0000

    Remove autheticate_user from items index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:show]
+  before_action :authenticate_user!, except: [:show, :index]
 
   def index
     @items = Item.all
